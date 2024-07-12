3973535701eb2e7f0f296c1783b73df2828c7655
Gourav soni <gouravsoni8476@gmail.com>
2024-07-12 19:14:55 +0530

    added listingController, listingRoute

---
+++ backend/src/controllers/listingController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+const newListingController = (req: Request, res: Response) => {};
+const getListingController = (req: Request, res: Response) => {};
+const updateListingController = (req: Request, res: Response) => {};
+const deleteListingController = (req: Request, res: Response) => {};
+export {
+  newListingController,
+  getListingController,
+  updateListingController,
+  deleteListingController,
+};
--- backend/src/routes/listingRoute.ts
+++ backend/src/routes/listingRoute.ts
@@ -1,4 +1,15 @@
 import express from "express";
 const listingRouter = express.Router();
+import {
+  newListingController,
+  getListingController,
+  updateListingController,
+  deleteListingController,
+} from "../controllers/listingController";
+
+listingRouter.post("/new", newListingController);
+listingRouter.get("/:id", getListingController);
+listingRouter.put("/:id", updateListingController);
+listingRouter.delete("/:id", deleteListingController);
 
 export default listingRouter;
